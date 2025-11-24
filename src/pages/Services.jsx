2f7d8ac14ade125403a2f951d03109af632cc50f
Pepe <josepp2508@gmail.com>
2025-11-24 06:11:21 -0600

    [FEAT] custom solutions showcase in '/services' route

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,11 +1,13 @@
 import { ServicesHero } from "../components/services/ServicesHero";
 import { EnterpriseSoftware } from "../components/services/EnterpriseSoftware";
+import { CustomSolutions } from "../components/services/CustomSolutions";
 
 export const Services = () => {
     return (
         <>
             <ServicesHero />
             <EnterpriseSoftware />
+            <CustomSolutions />
         </>
     )
 }
