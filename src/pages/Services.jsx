bda172879bd70686941c8d6293164fd00ce76701
Pepe <josepp2508@gmail.com>
2025-11-23 23:44:37 -0600

    [IMP] enterprise software section added

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,9 +1,11 @@
 import { ServicesHero } from "../components/services/ServicesHero";
+import { EnterpriseSoftware } from "../components/services/EnterpriseSoftware";
 
 export const Services = () => {
     return (
         <>
             <ServicesHero />
+            <EnterpriseSoftware />
         </>
     )
 }
